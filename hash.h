b1f21da4605585e66991511810df464a8d919b0b
Valeriy Boiko <vboiko@e1r2p2.unit.ua>
2017-02-23 20:40:57 +0200

    structs

--- hash.h
+++ hash.h
@@ -9,4 +9,25 @@
 # include <math.h>
 # include "./libft/printf_functions/includes/header.h"
 
+typedef struct      s_lst
+{
+    int             vid_amount;
+    int             ep_amount;
+    int             requests;
+    int             caches_amount;
+    int             cache_size;
+}                   desc_lst;
+
+typedef struct      s_ep
+{
+    int             *requests; //количество запросов с соответствующего эндпоинта
+    int             *cache;
+}                   ep_lst;
+
+typedef struct      s_vid
+{
+    int             *id;
+    int             *mem;
+}                   cache_lst;
+
 #endif
